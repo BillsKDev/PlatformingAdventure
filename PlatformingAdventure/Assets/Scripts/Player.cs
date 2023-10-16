9ab5269a1e7b2fb1be83dee6b57600cc30ec86dc
Bilal Khan <therealbills@hotmail.com>
2023-10-16 13:38:44 -0400

    Improved Jumping

--- PlatformingAdventure/Assets/Scripts/Player.cs
+++ PlatformingAdventure/Assets/Scripts/Player.cs
@@ -5,21 +5,34 @@ public class Player : MonoBehaviour
     [SerializeField] float _jumpVelocity = 5f;
     [SerializeField] float _jumpDuration = 0.5f;
     [SerializeField] float _horizontalVelocity = 3f;
+    [SerializeField] float _footOffset = 0.35f;
     [SerializeField] Sprite _jumpSprite;
+    [SerializeField] LayerMask _layerMask;
 
     Rigidbody2D _rb;
     SpriteRenderer _spriteRenderer;
     Animator _animator;
+    AudioSource _audioSource;
 
     float _jumpEndTime;
     float _horizontal;
+    int _jumpsRemaining;
     bool IsGrounded;
 
     void OnDrawGizmos()
     {
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
         Gizmos.color = Color.red;
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.bounds.extents.y);
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
+
+        //Draw left foot
+        origin = new Vector2(transform.position.x - _footOffset, transform.position.y - spriteRenderer.bounds.extents.y);
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
+
+        //Draw right foot
+        origin = new Vector2(transform.position.x + _footOffset, transform.position.y - spriteRenderer.bounds.extents.y);
         Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
     }
 
@@ -28,23 +41,23 @@ public class Player : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.bounds.extents.y);
-        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
-
-        if (hit.collider)
-            IsGrounded = true;
-        else
-            IsGrounded = false;
+        UpdateGrounding();
 
         _horizontal = Input.GetAxis("Horizontal");
         var vertical = _rb.velocity.y;
 
-        if (Input.GetButtonDown("Jump") && IsGrounded)
+        if (Input.GetButtonDown("Jump") && _jumpsRemaining > 0)
+        {
             _jumpEndTime = Time.time + _jumpDuration;
+            _jumpsRemaining--;
+            _audioSource.pitch = _jumpsRemaining > 0 ? 1 : 1.2f;
+            _audioSource.Play();
+        }
 
         if (Input.GetButtonDown("Jump") && _jumpEndTime > Time.time)
             vertical = _jumpVelocity;
@@ -53,6 +66,33 @@ public class Player : MonoBehaviour
         _rb.velocity = new Vector2(_horizontal, vertical);
 
         UpdateSprite();
+    }
+
+    void UpdateGrounding()
+    {
+        IsGrounded = false;
+        
+        //Check center
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.bounds.extents.y);
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
+        if (hit.collider)
+            IsGrounded = true;
+
+        //Check left
+        origin = new Vector2(transform.position.x - _footOffset, transform.position.y - _spriteRenderer.bounds.extents.y);
+        hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
+        if (hit.collider)
+            IsGrounded = true;
+
+        //Check right
+        origin = new Vector2(transform.position.x + _footOffset, transform.position.y - _spriteRenderer.bounds.extents.y);
+        hit = Physics2D.Raycast(origin, Vector2.down, 0.1f, _layerMask);
+        if (hit.collider)
+            IsGrounded = true;
+
+        if (IsGrounded && _rb.velocity.y <= 0)
+            _jumpsRemaining = 2;
+
     }
 
     void UpdateSprite()
@@ -64,7 +104,5 @@ public class Player : MonoBehaviour
             _spriteRenderer.flipX = false;
         else if (_horizontal < 0)
             _spriteRenderer.flipX = true;
-
-
     }
 }
