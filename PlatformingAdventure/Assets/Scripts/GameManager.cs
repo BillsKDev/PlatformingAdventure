f628194e9fcd600ba05b01b0e221139c8ee60a49
Bilal Khan <therealbills@hotmail.com>
2023-10-22 21:48:11 -0400

    Multiple Save Games

--- PlatformingAdventure/Assets/Scripts/GameManager.cs
+++ PlatformingAdventure/Assets/Scripts/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -6,6 +9,7 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] GameData _gameData;
     public static GameManager Instance { get; private set; }
+    public List<string> AllGameNames = new List<string>();
 
     PlayerInputManager _playerInputManager;
 
@@ -24,6 +28,10 @@ public class GameManager : MonoBehaviour
         _playerInputManager.onPlayerJoined += HandlePlayerJoined;
 
         SceneManager.sceneLoaded += HandleSceneLoaded;
+
+        string commaSeparatedList = PlayerPrefs.GetString("AllGameNames");
+        Debug.Log(commaSeparatedList);
+        AllGameNames = commaSeparatedList.Split(",").ToList();
     }
 
     void HandleSceneLoaded(Scene arg0, LoadSceneMode arg1)
@@ -41,13 +49,20 @@ public class GameManager : MonoBehaviour
     void SaveGame()
     {
         string text = JsonUtility.ToJson(_gameData);
-        PlayerPrefs.SetString("Game1", text);
+
+        if (AllGameNames.Contains(_gameData.GameName) == false)
+            AllGameNames.Add(_gameData.GameName);
+
+        string commaSeperatedGameNames = string.Join(",", AllGameNames);
+
+        PlayerPrefs.SetString("AllGameNames", commaSeperatedGameNames);
+        PlayerPrefs.SetString(_gameData.GameName, text);
         PlayerPrefs.Save();
     }
 
-    public void LoadGame()
+    public void LoadGame(string gameName)
     {
-        string text = PlayerPrefs.GetString("Game1");
+        string text = PlayerPrefs.GetString(gameName);
         _gameData = JsonUtility.FromJson<GameData>(text);
         SceneManager.LoadScene("Level 1");
     }
@@ -74,8 +89,9 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("New Game Called");
         _gameData = new GameData();
+        _gameData.GameName = DateTime.Now.ToString("G");
         SceneManager.LoadScene("Level 1");
     }
 
-    
+
 }
